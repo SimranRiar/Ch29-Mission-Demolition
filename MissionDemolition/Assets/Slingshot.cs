ad445c3e5cc337a0beb639f9af495e7252ad2527
SimranRiar <simriar@gmail.com>
2018-06-18 15:49:15 -0400

    5 - Launch Projectile

--- MissionDemolition/Assets/Slingshot.cs
+++ MissionDemolition/Assets/Slingshot.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour {
 
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
 
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+
+    private Rigidbody projectileRigidbody;
 
     void Awake()
     {
@@ -37,5 +40,38 @@ public class Slingshot : MonoBehaviour {
         projectile = Instantiate(prefabProjectile);
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos2D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+
+        }
     }
 }
