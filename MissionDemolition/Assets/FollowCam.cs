bfe219d1651c18048612a802f4786d5662809ee3
SimranRiar <simriar@gmail.com>
2018-06-19 09:39:56 -0400

    6 - Added Follow Camera

---
+++ MissionDemolition/Assets/FollowCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    static public GameObject POI;
+
+    public float camZ;
+
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination.z = camZ;
+
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
