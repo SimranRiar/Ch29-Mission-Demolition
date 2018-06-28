08dd6c1b2360a71b1f51c027243ad60d538760ed
SimranRiar <simriar@gmail.com>
2018-06-28 12:25:38 -0400

    7b - One Randomized Cloud

--- MissionDemolition/Assets/Cloud.cs
+++ MissionDemolition/Assets/Cloud.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    public GameObject cloudSphere;
+    public int numSpheresMin = 6;
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float scaleYMin = 2f;
+
+    private List<GameObject> spheres;
+
+    void Start()
+    {
+        spheres = new List<GameObject>();
+
+        int num = Random.Range(numSpheresMin, numSpheresMax);
+
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudSphere);
+            spheres.Add(sp);
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
+
+            Vector3 offset = Random.insideUnitSphere;
+            offset.x *= sphereOffsetScale.x;
+            offset.y *= sphereOffsetScale.y;
+            offset.z *= sphereOffsetScale.z;
+            spTrans.localPosition = offset;
+
+            Vector3 scale = Vector3.one;
+            scale.x = Random.Range(sphereScaleRangeX.x, sphereScaleRangeY.y);
+            scale.x = Random.Range(sphereScaleRangeY.x, sphereScaleRangeY.y);
+            scale.x = Random.Range(sphereScaleRangeZ.x, sphereScaleRangeY.y);
+
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
+            scale.y = Mathf.Max(scale.y, scaleYMin);
+
+            spTrans.localScale = scale;
+        }
+    }
+
+        /*void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Restart();
+            }
+        }*/
+
+        void Restart()
+        {
+            foreach (GameObject sp in spheres)
+            {
+                Destroy(sp);
+            }
+
+            Start();
+        }
+    }
+
